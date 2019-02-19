b11b9c3103ee9bb1fb8a67a6fa0022a3080bfaeb
Jez Higgins <jez@jezuk.co.uk>
2019-02-19 13:45:16 +0000

    Update for User Study network

--- archangel-ethereum-client.sh
+++ archangel-ethereum-client.sh
@@ -26,6 +26,11 @@ if [ "$ARCHANGEL_ETHEREUM_RPC_PORT" = "" ]; then
   ARCHANGEL_ETHEREUM_RPC_PORT=8545
 fi
 
+if [ "$ARCHANGEL_BOOTNODE" = "" ]; then
+  ARCHANGEL_BOOTNODE="enode://21ceb67bc34760d8776b8adc3a60ecd54c9c7d609c4e54ba4c3ca5c2de08df587d224826f3cae9114952027327f5d23100eb61451c4b2a5ff14139c50ff65f20@139.162.253.192:30310"
+fi
+
+
 for i in "$@"
 do
 case $i in
@@ -53,6 +58,10 @@ case $i in
   ARCHANGEL_ETHEREUM_ENABLE_RPC=false
   shift
   ;;
+  --bootnode)
+  ARCHANGEL_BOOTNODE="${i#*=}"
+  shift
+  ;;
   --)
   shift
   break
@@ -63,6 +72,7 @@ case $i in
 esac
 done
 
+ARCHANGEL_GENESIS_JSON="archangel-user-study-genesis.json"
 
 if [ "$ARCHANGEL_ETHEREUM_ENABLE_RPC" = "true" ]; then
   RPC_ARGS="--rpc --rpcaddr $ARCHANGEL_ETHEREUM_RPC_HOST --rpcport $ARCHANGEL_ETHEREUM_RPC_PORT --rpcapi 'personal,db,eth,net,web3,txpool,miner'"
@@ -70,14 +80,22 @@ fi
 
 # Initialise if necessary
 if [ ! -f "$ARCHANGEL_ETHEREUM_DATA_DIR/geth/chaindata/CURRENT" ]; then
-  $ARCHANGEL_ETHEREUM_GETH --datadir $ARCHANGEL_ETHEREUM_DATA_DIR init $SCRIPT_DIR/archangel-genesis.json
+  echo $ARCHANGEL_ETHEREUM_GETH --datadir $ARCHANGEL_ETHEREUM_DATA_DIR --networkid 53419 init $SCRIPT_DIR/$ARCHANGEL_GENESIS_JSON
+  $ARCHANGEL_ETHEREUM_GETH --datadir $ARCHANGEL_ETHEREUM_DATA_DIR --networkid 53419 init $SCRIPT_DIR/$ARCHANGEL_GENESIS_JSON
 fi
 
 # And off we go!
+echo $ARCHANGEL_ETHEREUM_GETH \
+  --datadir $ARCHANGEL_ETHEREUM_DATA_DIR \
+  --networkid 53419 \
+  --port $ARCHANGEL_ETHEREUM_PORT \
+  --bootnodes $ARCHANGEL_BOOTNODE \
+  $RPC_ARGS \
+  $1 $2 $3 $4 $5 $6 $7 $8 $9
 $ARCHANGEL_ETHEREUM_GETH \
   --datadir $ARCHANGEL_ETHEREUM_DATA_DIR \
-  --networkid 3151 \
+  --networkid 53419 \
   --port $ARCHANGEL_ETHEREUM_PORT \
-  --bootnodes enode://98d6401f594cd1e68de946680f009bcb504f61c5e755dc37e548c59b9dcc4651c30f74aaffb3f88c87ec240c763180a3ae113b125c6166dc23af8d3f6fd0a89c@172.104.142.159:30301 \
+  --bootnodes $ARCHANGEL_BOOTNODE \
   $RPC_ARGS \
   $1 $2 $3 $4 $5 $6 $7 $8 $9
